d75f221be0f601f62ae5c208b97455dac06cc00a
OliverJones08 <166547012+OliverJones08@users.noreply.github.com>
2025-05-14 15:07:16 -0300

    Initial commit

---
+++ Assets/Script.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Script : MonoBehaviour
+{
+    public TMP_InputField inputA;
+    public TMP_InputField inputB;
+    public TextMeshProUGUI txtAnswer;
+    int answer;
+    int numberA;
+    int numberB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtAnswer.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OperationAndResult()
+    {
+        numberA = int.Parse(inputA.text);
+        numberB = int.Parse(inputB.text);
+        answer = numberA + numberB;
+        txtAnswer.text = "La suma entre " + numberA + " y " + numberB + " y el resultado es " + answer;
+    }
+}
