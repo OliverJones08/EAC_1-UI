d18352907219acf34140b50caab830699df21878
OliverJones08 <166547012+OliverJones08@users.noreply.github.com>
2025-05-21 15:50:32 -0300

    Comida Animalitos - Repaso UI
    Codigo terminado, solo falta cambiarlo a la version 2023 2.2 y hacer el UI con Text Mesh Pro y conectar el codigo con la UI.

---
+++ Assets/Scripts/Par o impar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Paroimpar : MonoBehaviour
+{
+    public TMP_InputField imputnum1;
+    public TextMeshProUGUI txtParOImpar;
+    int num1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtParOImpar.text = string.Empty;
+    }
+
+    public void EsParOEsImpar()
+    {
+        if (inputNum1.text == "")
+        {
+            txtParOImpar.text = "El campo esta vacio";
+        }
+        num1 = int.Parse(inputNum1.text);
+
+        if(num1 <= 0)
+        {
+            txtParOImpar.text = "El numero debe ser mayor que cero";
+        }
+
+        if (num1 % 2 == 0)
+        {
+            txtParOImpar.text = "El numero proporcionado es par";
+        }
+
+        else {
+            txtParOImpar.text = "El numero proporcionado es impar";
+        }
+    }
+}
